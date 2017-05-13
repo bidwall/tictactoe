cfbf9737235c29291a761a521a552889a0cc74fb
Palvinder Bidwall <pal@bidwall.co.uk>
2017-05-13 12:21:18 +0100

    Move calculation of winner into seperate js file

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Board from './board/Board';
+import { getWinner } from './utility.js';
+
 
 export default class Game extends React.Component {
   constructor() {
@@ -17,7 +19,7 @@ export default class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    if (getWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -40,7 +42,7 @@ export default class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
+    const winner = getWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ? 'Move #' + move : 'Game start';
@@ -70,24 +72,4 @@ export default class Game extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
